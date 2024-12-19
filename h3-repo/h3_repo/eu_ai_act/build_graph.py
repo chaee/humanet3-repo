9c178a3e98c3efb1b53abeb0ab38d3ef881f18fd
Yun <yun@mpib-berlin.mpg.de>
2024-12-19 14:24:03 +0100

    change urls with url info (doc type, year, etc.) and then query that url to retrieve CELEX number so that html file can be retrieved

--- h3-repo/h3_repo/eu_ai_act/build_graph.py
+++ h3-repo/h3_repo/eu_ai_act/build_graph.py
@@ -1,8 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
 import networkx as nx
-from extract_url import find_citation, extract_urls_from_html
+from extract_url import extract_urls_from_html
 
+def read_url_to_html(url, doc_id):
+    # html link: https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32008R0300
+    response = requests.get(url)
+    if response.status_code == 200:
+        html_content = response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        document_title = soup.find('p', class_='DocumentTitle pull-left')
+        if document_title:
+            CELEX_num = document_title.text.strip()[9:]
+            print(CELEX_num)
+        
+        #with open(f'/Users/yun/Dev/humanet3/human-centered-repo/h3-repo/h3_repo/eu_ai_act/references/{doc_id}.html', 'w') as file:
+        #    file.write(html_content)
+        #print("HTML content saved to 'downloaded_page.html'")
+    else:
+        print('error')
+          
 # html_path = 'h3-repo/h3_repo/eu_ai_act/L_202401689EN.000101.fmx.xml.html'
 html_path = '/Users/yun/Dev/humanet3/human-centered-repo/h3-repo/h3_repo/eu_ai_act/L_202401689EN.000101.fmx.xml.html'
-
 urls = extract_urls_from_html(html_path)
-find_citation(urls)
+
+with open('eu_ai_act_refs.csv', 'w') as file:
+    for url in urls:
+        file.write(url + '\n')
+
+for url in urls:
+    doc_id = url.split('uriserv:')[1].split('&')[0]
+    read_url_to_html(url, doc_id)
--- h3-repo/h3_repo/eu_ai_act/extract_url.py
+++ h3-repo/h3_repo/eu_ai_act/extract_url.py
@@ -1,36 +1,45 @@
 # open html file and extract urls
 
-import os
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
-import requests
 
 def extract_urls_from_html(html_file):
+    completed_urls = []
     with open(html_file, 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
-    urls = {}
+    all_urls = {} # url : page data pair (ex: https://eur-lex.europa.eu/legal-content/EN/AUTO/?uri=OJ:C:2021:517:TOC & OJ C 517, 22.12.2021, p. 56)
     for link in soup.find_all('a'):
-        urls[link.get('href')] = link.get_text()
-    return urls
-
-
-def find_citation(url_data):
+        all_urls[link.get('href')] = link.get_text().strip()
+        
     # find relevant urls
     # exclude urls including "https://data.europa.eu/eli/reg/2024/1689/oj" or "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=OJ:L_202401689" (referring to EU AI Act itself)
     # iterate the dictionary url_data and leave only  that does not contain the above urls
-    valid_pair = {}
-    for url in url_data.keys():
+    print("all urls:", len(all_urls))
+    for url in all_urls.keys():
         if 'https://data.europa.eu/eli/reg/2024/1689/oj' not in url and 'https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=OJ:L_202401689' not in url:
-            valid_pair[url] = url_data[url]
-            print(url, '--', url_data[url])
-    return valid_pair
-             
-
-    # urls = url_data['url']
-    # urls = [url for url in urls if 'https://data.europa.eu/eli/reg/2024/1689/oj' not in url and 'https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=OJ:L_202401689' not in url]
-    
-    # df = pd.DataFrame(urls, columns=['url'])
-    # df.to_csv('h3_repo/eu_ai_act/urls.csv', index=False)
+            completed_urls.append(complete_url(url, all_urls[url]))
+    print("after removing self-reference:", len(completed_urls))
+    return completed_urls
 
 
+def complete_url(url, url_data):
+    pattern = r'OJ\s(?P<doc_type>\w+)\s(?P<doc_num>\d+),\s(?P<date>\d{1,2}\.\d{1,2}\.\d{4}),\sp\.\s(?P<page_num>\d+)'
+    match = re.search(pattern, url_data)
+    if match:
+        doc_type = match.group("doc_type")
+        doc_num = f'{int(match.group("doc_num")):03}'
+        date = match.group("date")
+        page_num = f'{int(match.group("page_num")):04}'
+        final_url = f'https://eur-lex.europa.eu/legal-content/EN/AUTO/?uri=uriserv:OJ.{doc_type}_.{date.split(".")[2]}.{doc_num}.01.{page_num}.01.ENG&toc=OJ:{doc_type}:{date.split(".")[2]}:{doc_num}:TOC'
+    #    print('matched:', url_data)
+    else:
+        #print(f'>{url_data}<')
+        # print('not matched:', url_data)
+        final_url = url
+    return final_url
+
+# # complete_url("https://eur-lex.europa.eu/legal-content/EN/AUTO/?uri=OJ:L:2018:151:TOC", "OJ L 151, 14.6.2018, p. 1")
+# html_path = '/Users/yun/Dev/humanet3/human-centered-repo/h3-repo/h3_repo/eu_ai_act/L_202401689EN.000101.fmx.xml.html'
+# urls = extract_urls_from_html(html_path)
+# print(urls)
